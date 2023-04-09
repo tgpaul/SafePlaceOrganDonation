2da1f17b6dc442b4e3c078c591b22ace5fb541e8
Pranav-Official <pranavhtjp07@gmail.com>
2023-04-10 00:24:34 +0530

    Updated Match found Componet
    The match found componet inside the donor dashboard was updated with better design, added icons, and slot cards, readable code

--- Components/DonorDashboard/DonorMatch_Found.js
+++ Components/DonorDashboard/DonorMatch_Found.js
@@ -1,14 +1,47 @@
-let Match_not_found = "/match_not_found.svg";
+let phone_icon = "/recipient_slot_icons/phone.svg"
+let id_icon = "/recipient_slot_icons/id.svg"
+let blooddrop_icon = "/recipient_slot_icons/blood_drop_.svg"
+let heart_icon = "/recipient_slot_icons/heart.svg"
+let hospital_icon = "/recipient_slot_icons/hospital.svg"
+let phoneHospital_icon = "/recipient_slot_icons/phoneHospital.svg"
+let profile_pic = "/profile.svg"
 
 function DonorMatch_Found(){
 
     return (
         <form id="form2">
-          <div className="form-control">
-            <h1>Matching Details</h1>
-            <div className="image-box">
-              <img src={Match_not_found} alt="Example image" />
-              <p>Match Found!</p>
+          <div className="card-control">
+            <h1 className="card-heading">Matching Details</h1>
+            <div className="recipientCard">
+                <h1>RECIPIENT DETAILS</h1>
+                <img src= {profile_pic} alt="Donor Photo" />
+                <h3>Princy Sugahthan</h3>
+                <div className="detail_slots">
+                    <div className="slot">
+                        <div className="slot_icon"><img src= {phone_icon} width="30" alt="phone_icon" /></div>
+                        <p className="slot_text">+91 83838 12131</p>
+                    </div>
+                    <div className="slot">
+                        <div className="slot_icon"><img src= {id_icon} width="30" alt="ID_icon" /></div>
+                        <p className="slot_text">#cau8cua888sss</p>
+                    </div>
+                    <div className="slot">
+                        <div className="slot_icon"><img src= {blooddrop_icon} width="30" alt="blood_drop icon" /></div>
+                        <p className="slot_text">AB Positive</p>
+                    </div>
+                    <div className="slot">
+                        <div className="slot_icon"><img src= {heart_icon} width="30" alt="heart icon" /></div>
+                        <p className="slot_text">Heart, Liver</p>
+                    </div>
+                    <div className="slot">
+                        <div className="slot_icon"><img src= {hospital_icon} width="30" alt="hospital_icon" /></div>
+                        <p className="slot_text">Baby Memorial Hospital</p>
+                    </div>
+                    <div className="slot">
+                        <div className="slot_icon"><img src= {phoneHospital_icon} width="30" alt="phone hospital_icon" /></div>
+                        <p className="slot_text">+91 91231 21321</p>
+                    </div>
+                </div>
             </div>
           </div>
         </form>
--- Components/DonorDashboard/DonorMatch_NotFound.js
+++ Components/DonorDashboard/DonorMatch_NotFound.js
@@ -5,7 +5,7 @@ function DonorMatch_NotFound(){
     return (
         <form id="form2">
           <div className="form-control">
-            <h1>Matching Details</h1>
+          <h1 className="card-heading">Matching Details</h1>
             <div className="image-box">
               <img src={Match_not_found} alt="Example image" />
               <p>Match Not Yet Found!</p>
