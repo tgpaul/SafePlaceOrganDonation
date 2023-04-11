0719f06855de048e83c1cf858b8b1f5e10c80bfb
Pranav-Official <pranavhtjp07@gmail.com>
2023-04-12 02:26:46 +0530

    test commit moralis

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -3,7 +3,7 @@ import { MoralisNextApi } from "@moralisweb3/next";
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY,
   authentication: {
-    domain: "localhost:3000",
+    domain: "safeplaceod.vercel.app",
     uri: process.env.NEXTAUTH_URL,
     timeout: 120,
   },
