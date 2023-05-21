24d3f267bb7ae3b873bc8da62331f05d4f3cc034
tgpaul <tgpaul9801@gmail.com>
2023-05-21 15:49:31 +0530

    Connection first fix
    This is a commit right after asking Raza for the fix

--- Components/BackendFunctions/DonorFunctions.js
+++ Components/BackendFunctions/DonorFunctions.js
@@ -1,16 +1,42 @@
-const {donorContract, hospitalRecipientContract} = require("./ContarctObjects");
+const Web3 = require('web3');
+const DonorContract = require('../../blockchain/build-info/DonorContract.json');
 
-/*
-    Functions: - 
-    SignUp
-*/
+let accounts, web3, donorContract;
 
-async function SignUpFunction( donorAddress, firstname, lastname, contact, email, resAddress ) {
-    try {
-        const ID = await donorContract.methods.DonorSignUp(firstname, lastname,contact, email,resAddress).send({from: donorAddress});
-    } catch (error) {
-        console.error('Error creating donor:', error);
+async function SignUpFunction( firstname, lastname, contact, email, resAddress ){
+try{
+    // popup - get the user's address
+    accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+    // using web3 just as a helper to generate the transaction
+    // (see the `data` field and `encodeABI`) - not to sign it
+    web3 = new Web3();
+    donorContract = new web3.eth.Contract(DonorContract.abi, DonorContract.address);
+
+    const transactionParameters = {
+        from: accounts[0],
+        to: DonorContract.address,
+        data: donorContract.methods.DonorSignUp(
+            firstname,
+            lastname,
+            contact,
+            email,
+            resAddress
+        ).encodeABI(),
+        gasPrice: '0x09184e72a000', // custom gas price
+    };
+
+     // popup - request the user to sign and broadcast the transaction
+     await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [transactionParameters],
+    });
+
+    console.log("Transaction Complete");
+    } catch(error){
+        console.log("Error: ",error);
     }
+    
 }
 
 export default SignUpFunction;
